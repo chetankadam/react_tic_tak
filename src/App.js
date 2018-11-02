0aa3e1ce1702bb1430c769f35529f72efde04de1
chetan kadam <chetan.kadam@aasaanjobs.com>
2018-11-02 12:09:39 +0530

    basic struture added

--- src/App.js
+++ src/App.js
@@ -2,27 +2,75 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+class Square extends React.Component {
+  render() {
+    console.log("this is", this);
+    return (
+      <button className="square" >
+        {this.props.value}
+      </button>
+    )
+  }
+}
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square/>;
+  }
+
+  render(){
+    const status = 'Next Player: X';
+    return(
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    )
+  }
+}
+class Game extends React.Component {
+  render(){
+    return (
+      <div>
+        <div className="game">
+          <div className="game-board">
+            <Board/>
+          </div>
+          <div>
+            {/* Status */}
+          </div>
+          <ol>
+            {/* TODO */}
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Game/>
     );
   }
+
+
+  
 }
 
 export default App;
